68fb490a77c31e1d69a02c65021ccefcc6c037ee
Anoqu0n <2578932018@qq.com>
2024-11-20 11:36:45 +0800

    初始化

--- src/main/java/Agent/agent.java
+++ src/main/java/Agent/agent.java
@@ -1,8 +1,13 @@
 package Agent;
 
+import java.lang.instrument.Instrumentation;
+
 /**
  * @author 0x安权
  * 2024/11/20 11:13
  */
 public class agent {
+    public static void premain(String agentArgs, Instrumentation inst) {
+
+    }
 }
--- src/main/java/config/console.java
+++ src/main/java/config/console.java
@@ -4,5 +4,9 @@ package config;
  * @author 0x安权
  * 2024/11/20 11:18
  */
+
+//日志
 public class console {
+
+
 }
